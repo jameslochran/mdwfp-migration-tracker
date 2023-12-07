babb30a93af8fa289d04dc5602797fad19bce5eb
James Lochran <jlochran@gmail.com>
2023-12-07 13:42:14 +0000

    Add data and add dynamic table element

--- mdwfpm.py
+++ mdwfpm.py
@@ -2,16 +2,32 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
 st.title('MDWFP Migration Tracker')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+
+
+DATA_URL = ('MDWFP_import.csv')
+
+df = pd.read_csv(DATA_URL)
+users = ['Jim', 'Bob', 'Sally']
+config = {
+    'Assignment' : st.column_config.SelectboxColumn('Name', options=users)
+}
+df['In Progress'] = False
+df['Content Review'] = False
+df['Client Review'] = False
+df['Done'] = False
+
+
+       
+
+
+
+
+st.subheader('Raw data')
+
+# edited_df = st.data_editor(df)
+st.data_editor(df, column_config=config)
+
+
