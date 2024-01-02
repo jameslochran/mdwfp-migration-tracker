63103e89edef45d30e112d1f978c73a64c73d551
James Lochran <jlochran@gmail.com>
2024-01-02 01:11:47 +0000

    Added time stamp columns

--- count.py
+++ count.py
@@ -21,5 +21,17 @@ def count_slashes_in_urls(df, url_column, new_column_name="num_slashes"):
 
 df = pd.read_csv('folder/out.csv').astype(str) 
 # df['new_column_name'] = df['url'].apply(lambda url: len(re.findall(r"/\/", 'url')))
-df['count'] = df['Legacy URL'].apply(lambda x: x.count('/'))
+# df['count'] = df['Legacy URL'].apply(lambda x: x.count('/'))
+# df['IP'] = df['IP'].astype('datetime64[ns]')
+# df['Content Review'] = pd.to_datetime(df['Content Review'])
+# df['Client Review'] = pd.to_datetime(df['Client Review'])
+# df['Done'] = pd.to_datetime(df['Done'])
+df['IP'] = pd.Timestamp('now')
+df['Content Review'] = pd.Timestamp('now')
+df['client Review'] = pd.Timestamp('now')
+df['Done'] = pd.Timestamp('now')
+
+
+
+print(df)
 df.to_csv('folder/out.csv', index=False) 
